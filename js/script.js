19ee7397578526ae2ee7caf057f07acd87beeb6e
Jenny Ho <jennyho@Jennys-MacBook-Pro.local>
2023-02-15 23:30:15 +0000

    update of js css html

--- js/script.js
+++ js/script.js
@@ -1,16 +1,82 @@
-// Retrieve the current vote counts from local storage, or initialize them if they don't exist
-let candidates = JSON.parse(localStorage.getItem("candidates")) || {"A": 0, "B": 0, "C": 0};
+// Retrieve the vote counts from local storage
+let voteCounts = {};
+if (localStorage.getItem('voteCounts')) {
+  voteCounts = JSON.parse(localStorage.getItem('voteCounts'));
+} else {
+  voteCounts = {
+    A: 0,
+    B: 0,
+    C: 0
+  };
+}
 
-// Function to handle the user's vote
-function vote(candidate) {
-  if (candidates.hasOwnProperty(candidate)) {
-    candidates[candidate] += 1;
-    console.log(`You voted for ${candidate}.`);
-    localStorage.setItem("candidates", JSON.stringify(candidates));
+// Update the vote count paragraph for each candidate
+document.querySelectorAll('.candidate').forEach((element) => {
+  const candidateName = element.querySelector('h2').textContent.trim();
+  const voteCountElement = element.querySelector('span');
+  voteCountElement.textContent = voteCounts[candidateName];
+});
+
+// Function to handle voting for a candidate
+function vote(candidateName) {
+  // Update the vote count for the selected candidate
+  if (voteCounts.hasOwnProperty(candidateName)) {
+    voteCounts[candidateName]++;
+    
+    // Save the updated vote counts to local storage
+    localStorage.setItem('voteCounts', JSON.stringify(voteCounts));
+    
+    // Update the vote count paragraph for the selected candidate
+    const candidateElement = document.querySelector(`h2:contains(${candidateName})`).parentElement;
+    const voteCountElement = candidateElement.querySelector('span');
+    voteCountElement.textContent = voteCounts[candidateName];
   } else {
-    console.log(`Sorry, ${candidate} is not a valid candidate.`);
+    console.error(`Candidate '${candidateName}' not found.`);
   }
 }
 
-// Call the vote function with the user's choice
-vote("A");  // Example vote for candidate A
+// Function to display the result page
+function showResult() {
+  // Retrieve the vote counts from local storage
+  const voteCounts = JSON.parse(localStorage.getItem('voteCounts'));
+  
+  // Find the candidate with the highest vote count
+  let winner = null;
+  let highestVoteCount = 0;
+  for (const candidateName in voteCounts) {
+    if (voteCounts[candidateName] > highestVoteCount) {
+      winner = candidateName;
+      highestVoteCount = voteCounts[candidateName];
+    }
+  }
+  
+  // Display the result page
+  document.querySelector('#result-page').classList.remove('hidden');
+  document.querySelector('#vote-page').classList.add('hidden');
+  document.querySelector('#winner').textContent = `The winner is ${winner} with ${highestVoteCount} votes!`;
+  
+  // Update the vote count table on the result page
+  const table = document.querySelector('#vote-count-table');
+  for (const candidateName in voteCounts) {
+    const row = table.insertRow();
+    const nameCell = row.insertCell(0);
+    const countCell = row.insertCell(1);
+    nameCell.textContent = candidateName;
+    countCell.textContent = voteCounts[candidateName];
+  }
+}
+
+// Event listener for the vote buttons
+document.querySelectorAll('.vote-button').forEach((element) => {
+  element.addEventListener('click', () => {
+    const candidateName = element.getAttribute('data-candidate');
+    vote(candidateName);
+  });
+});
+
+// Event listener for the show result button
+document.querySelector('#show-result-button').addEventListener('click', () => {
+  showResult();
+});
+
+
